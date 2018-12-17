a2a069fb4b2481326237fce93cf5d86245ab24b6
Miles <872564905@qq.com>
2018-12-17 14:37:33 +0800

    "update codes"

--- src/main/java/com/baizhi/demo2/controller/UserController.java
+++ src/main/java/com/baizhi/demo2/controller/UserController.java
@@ -2,7 +2,6 @@ package com.baizhi.demo2.controller;
 
 import com.baizhi.demo2.entity.User;
 import com.baizhi.demo2.mapper.UserMapper;
-import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,6 +33,10 @@ public class UserController {
         for (User user : users) {
             System.out.println(user);
         }
+        System.out.println("ewqeqw");
+
+
+
         return users;
     }
 
